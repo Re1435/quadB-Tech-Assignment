248c67f9b9b5275e0422fca173a9d8314de5d65c
Re1435 <reddymahereddy143@gmail.com>
2024-04-02 12:03:33 +0530

    commit the todo code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+import { useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
+  const theme = useSelector((state) => state.theme);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme === "light" ? "light-mode" : "dark-mode"}`}>
+      <h1>Todo List</h1>
+      <div className='todo-list'>
+        <TaskInput />
+        <TaskList />
+      </div>
     </div>
   );
 }
